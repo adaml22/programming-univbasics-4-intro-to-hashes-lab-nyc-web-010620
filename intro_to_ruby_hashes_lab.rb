9483111f5cf91dca79805600f9c6a894d80e1cb2
Adam Longman <boardhead89@gmail.com>
2019-12-30 20:19:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -4,28 +4,30 @@ end
 
 def my_hash
   my_hash = {
-    :name => "Adam"
+    name: "Adam"
   }
 end
 
 def pioneer
   pioneer = {
-    :name => "Grace Hopper"
+    name: "Grace Hopper"
   }
 end
 
 def id_generator
   id_generator = {
-    :id => 4
+    id: 4
   }
 end
 
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+  argument_hash = {
+    key: value
+  }
 end
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+  return hash[key]
 end
 
 def update_counting_hash(hash, key)
