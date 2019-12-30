998641203889f3137141fc939cce8f89f36c87ce
Adam Longman <boardhead89@gmail.com>
2019-12-30 20:35:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,21 +20,21 @@ def id_generator
   }
 end
 
-def my_hash_creator(:key, value)
+def my_hash_creator(key, value)
   argument_hash = {
     key: value
   }
 end
 
-def read_from_hash(hash, :key)
+def read_from_hash(hash, key)
   return hash
 end
 
-def update_counting_hash(hash, :key)
-  if hash[key]
-    hash[key] += 1
+def update_counting_hash(hash, key)
+  if hash[:key]
+    hash[:key] += 1
   else
-    hash[key] = 1
+    hash[:key] = 1
   end
   return hash
 end
